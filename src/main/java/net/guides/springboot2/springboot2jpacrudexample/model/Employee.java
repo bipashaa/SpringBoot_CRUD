3981f99876b02ad16081359578e32ebe8ca0ee57
Bipasha Dutta <bipashaaduttaa@gmail.com>
2023-01-03 20:15:16 +0530

    Spring CRUD

---
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/model/Employee.java
@@ -0,0 +1,71 @@
+package net.guides.springboot2.springboot2jpacrudexample.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+
+	public Employee() {
+
+	}
+
+	public Employee(int id, String firstName, String lastName, String emailId) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+
+	@Id
+	@Column(name = "id", nullable = false)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(name = "firstname", nullable = false)
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	@Column(name = "lastname", nullable = false)
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Column(name = "email", nullable = false)
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
+				+ "]";
+	}
+
+}
